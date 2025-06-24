23f4a17e180be3d6ab3f18f7040ad63fbcd4f5a2
kaiser-shah <shah.docklands@gmail.com>
2025-06-24 14:57:17 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import AuthPage from "./pages/AuthPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProfilePage from "./pages/ProfilePage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/login" element={<AuthPage />} />
+        <Route path="*" element={<AuthPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
